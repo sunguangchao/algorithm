238676318ec4bd7badbc4a44121e4dcafbbafa1b
孙光超 <1198125767@qq.com>
2016-12-29 08:00:21 +0800

    Update QuickFindUF.java

--- algorithms_4th_edition/web_exercise1_5/QuickFindUF.java
+++ algorithms_4th_edition/web_exercise1_5/QuickFindUF.java
@@ -36,9 +36,12 @@ public class QuickFindUF {
     public void union(int p, int q){
         validate(p);
         validate(q);
+        //将p和q归并到相同的分量中
         int pID = id[p];
         int qID = id[q];
+        //如果p和q已经在相同的分量中则不需要采取任何行动
         if (pID == qID) return;
+        //将p的分量重命名为q的名称
         for (int i=0; i < id.length; i++)
             if (id[i] == pID) id[i] = qID;
         count--;
